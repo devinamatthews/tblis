168d9a39f5469cc318941f914f3fbab594b60440
Devin Matthews <dmatthews@utexas.edu>
2017-05-31 15:10:40 -0500

    Clean up TBB in partm and enable for tensor contraction.

--- src/nodes/packm.hpp
+++ src/nodes/packm.hpp
@@ -34,16 +34,14 @@ struct pack_row_panel
                                     : cfg.gemm_nr.extent<T>());
         const len_type KR = cfg.gemm_kr.def<T>();
 
-        len_type m_a = A.length( Trans);
-        len_type k_a = A.length(!Trans);
-        stride_type rs_a = A.stride( Trans);
-        stride_type cs_a = A.stride(!Trans);
+        const len_type m_a = A.length( Trans);
+        const len_type k_a = A.length(!Trans);
+        const stride_type rs_a = A.stride( Trans);
+        const stride_type cs_a = A.stride(!Trans);
 
 #if TBLIS_ENABLE_TBB
 
-#if TBLIS_SIMPLE_TBB
         tbb::task_group tg;
-#endif
 
         // Decompose into coarse-grained blocks
         // TODO: May need tuning
@@ -54,17 +52,15 @@ struct pack_row_panel
         for (unsigned tid = 0;tid < nthread;tid++)
         {
 
-#if TBLIS_SIMPLE_TBB
         tg.run([&,tid]
         {
-#endif
 
-#else
+#else //TBLIS_ENABLE_TBB
 
         unsigned nthread = comm.num_threads();
         unsigned tid = comm.thread_num();
 
-#endif
+#endif //TBLIS_ENABLE_TBB
 
         len_type m_first, m_last, k_first, k_last;
         std::tie(m_first, m_last, std::ignore,
@@ -90,17 +86,13 @@ struct pack_row_panel
 
 #if TBLIS_ENABLE_TBB
 
-#if TBLIS_SIMPLE_TBB
         });
-#endif
 
         }
 
-#if TBLIS_SIMPLE_TBB
         tg.wait();
-#endif
 
-#endif
+#endif //TBLIS_ENABLE_TBB
 
     }
 
@@ -114,24 +106,43 @@ struct pack_row_panel
                                     : cfg.gemm_nr.extent<T>());
         const len_type KR = cfg.gemm_kr.def<T>();
 
-        len_type m_a = A.length( Trans);
-        len_type k_a = A.length(!Trans);
-        stride_type rs_a = A.stride( Trans);
-        stride_type cs_a = A.stride(!Trans);
-        const stride_type* rscat_a = A.scatter( Trans);
-        const stride_type* cscat_a = A.scatter(!Trans);
-        const T* p_a = A.data();
-        T* p_ap = Ap.data();
+        const len_type m_a = A.length( Trans);
+        const len_type k_a = A.length(!Trans);
+        const stride_type rs_a = A.stride( Trans);
+        const stride_type cs_a = A.stride(!Trans);
+
+#if TBLIS_ENABLE_TBB
+
+        tbb::task_group tg;
+
+        // Decompose into coarse-grained blocks
+        // TODO: May need tuning
+        const len_type MR_BS = 2*MR;
+        const len_type KR_BS = 8*KR;
+        unsigned nthread = ((m_a+MR_BS-1)/MR_BS) * ((k_a+KR_BS-1)/KR_BS);
+
+        for (unsigned tid = 0;tid < nthread;tid++)
+        {
+
+        tg.run([&,tid]
+        {
+
+#else //TBLIS_ENABLE_TBB
+
+        unsigned nthread = comm.num_threads();
+        unsigned tid = comm.thread_num();
+
+#endif //TBLIS_ENABLE_TBB
 
         len_type m_first, m_last, k_first, k_last;
         std::tie(m_first, m_last, std::ignore,
                  k_first, k_last, std::ignore) =
-            comm.distribute_over_threads_2d(m_a, k_a, MR, KR);
+            communicator::distribute_2d(nthread, tid, m_a, k_a, MR, KR);
 
-        p_a += m_first*rs_a + k_first*cs_a;
-        rscat_a += m_first;
-        cscat_a += k_first;
-        p_ap += m_first*k_a + k_first*ME;
+        const stride_type* rscat_a = A.scatter( Trans) + m_first;
+        const stride_type* cscat_a = A.scatter(!Trans) + k_first;
+        const T*  p_a =  A.data() +  m_first       *rs_a + k_first*cs_a;
+              T* p_ap = Ap.data() + (m_first/MR)*ME* k_a + k_first*  ME;
 
         for (len_type off_m = m_first;off_m < m_last;off_m += MR)
         {
@@ -177,11 +188,21 @@ struct pack_row_panel
 
             p_ap += ME*k_a;
         }
+
+#if TBLIS_ENABLE_TBB
+
+        });
+
+        }
+
+        tg.wait();
+
+#endif //TBLIS_ENABLE_TBB
     }
 
     template <typename Communicator>
     void operator()(Communicator& comm, const config& cfg,
-                    block_scatter_matrix<T> A, matrix_view<T>& Ap) const
+                    block_scatter_matrix<T> A_, matrix_view<T>& Ap) const
     {
         const len_type MR = (!Trans ? cfg.gemm_mr.def<T>()
                                     : cfg.gemm_nr.def<T>());
@@ -192,16 +213,40 @@ struct pack_row_panel
         TBLIS_ASSERT(A.block_size(0) == (!Trans ? MR : KR));
         TBLIS_ASSERT(A.block_size(1) == (!Trans ? KR : MR));
 
-        len_type m_a = A.length( Trans);
-        len_type k_a = A.length(!Trans);
-        T* p_ap = Ap.data();
+        const len_type m_a = A_.length( Trans);
+        const len_type k_a = A_.length(!Trans);
+
+#if TBLIS_ENABLE_TBB
+
+        tbb::task_group tg;
+
+        // Decompose into coarse-grained blocks
+        // TODO: May need tuning
+        const len_type MR_BS = 2*MR;
+        const len_type KR_BS = 8*KR;
+        unsigned nthread = ((m_a+MR_BS-1)/MR_BS) * ((k_a+KR_BS-1)/KR_BS);
+
+        for (unsigned tid = 0;tid < nthread;tid++)
+        {
+
+        tg.run([&,tid]
+        {
+
+        auto A = A_;
+
+#else //TBLIS_ENABLE_TBB
+
+        unsigned nthread = comm.num_threads();
+        unsigned tid = comm.thread_num();
+
+        auto& A = A_;
+
+#endif //TBLIS_ENABLE_TBB
 
         len_type m_first, m_last, k_first, k_last;
         std::tie(m_first, m_last, std::ignore,
                  k_first, k_last, std::ignore) =
-            comm.distribute_over_threads_2d(m_a, k_a, MR, KR);
-
-        p_ap += m_first*k_a + k_first*ME;
+            communicator::distribute_2d(nthread, tid, m_a, k_a, MR, KR);
 
         len_type off_m = m_first;
 
@@ -209,6 +254,7 @@ struct pack_row_panel
         A.shift(Trans, off_m);
 
         const T* p_a = A.raw_data();
+        T* p_ap = Ap.data() + (m_first/MR)*ME*k_a + k_first*ME;
         const stride_type* cscat_a = A.scatter(!Trans) + k_first;
         const stride_type* cbs_a = A.block_scatter(!Trans) + k_first/KR;
 
@@ -242,6 +288,16 @@ struct pack_row_panel
 
         A.shift(Trans, -off_m);
         A.length(Trans, m_a);
+
+#if TBLIS_ENABLE_TBB
+
+        });
+
+        }
+
+        tg.wait();
+
+#endif //TBLIS_ENABLE_TBB
     }
 };
 
--- src/nodes/partm.hpp
+++ src/nodes/partm.hpp
@@ -24,7 +24,7 @@ struct partition
 
     template <typename T, typename Communicator, typename MatrixA, typename MatrixB, typename MatrixC>
     void operator()(Communicator& comm, const config& cfg,
-                    T alpha, MatrixA& A_, MatrixB& B_, T beta_, MatrixC& C_)
+                    T alpha, MatrixA& A, MatrixB& B, T beta, MatrixC& C)
     {
         using namespace matrix_constants;
 
@@ -37,31 +37,84 @@ struct partition
 
         TBLIS_ASSERT(M_ext == M_def);
 
-        len_type m_u = (Dim == DIM_M ? A_.length(0) : Dim == DIM_N ? B_.length(1) : A_.length(1));
-        len_type m_v = (Dim == DIM_M ? C_.length(0) : Dim == DIM_N ? C_.length(1) : B_.length(0));
-        len_type m = std::min(m_u, m_v);
+        const len_type m_u = (Dim == DIM_M ? A.length(0) :
+                              Dim == DIM_N ? B.length(1) : A.length(1));
+        const len_type m_v = (Dim == DIM_M ? C.length(0) :
+                              Dim == DIM_N ? C.length(1) : B.length(0));
+        const len_type m = std::min(m_u, m_v);
+
+        auto body = [&,this](int tid, Child& child, MatrixA& A, MatrixB& B, MatrixC& C)
+        {
+            len_type m_first, m_last;
+            std::tie(m_first, m_last, std::ignore) =
+                communicator::distribute(distribute, tid, m, M_iota);
+
+            auto length = [&](len_type m_u, len_type m_v)
+            {
+                (Dim == DIM_M ? A.length(0, m_u) :
+                 Dim == DIM_N ? B.length(1, m_u) : A.length(1, m_u));
+                (Dim == DIM_M ? C.length(0, m_v) :
+                 Dim == DIM_N ? C.length(1, m_v) : B.length(0, m_v));
+            };
+
+            auto shift = [&](len_type m_u, len_type m_v)
+            {
+                (Dim == DIM_M ? A.shift(0, m_u) :
+                 Dim == DIM_N ? B.shift(1, m_u) : A.shift(1, m_u));
+                (Dim == DIM_M ? C.shift(0, m_v) :
+                 Dim == DIM_N ? C.shift(1, m_v) : B.shift(0, m_v));
+            };
+
+            len_type m_off = m_first;
+            len_type m_len = m_last-m_first;
+
+            shift(m_off, m_off);
+
+            len_type M_cur = (m_len%M_def <= M_over ? M_max : M_def);
+
+            while (m_off < m_last)
+            {
+                len_type m_loc = std::min(m_last-m_off, M_cur);
+                length(m_loc, m_loc);
+
+                child(subcomm, cfg, alpha, A, B, beta, C);
+                if (Dim == DIM_K) beta = 1.0;
+
+                shift(M_cur, M_cur);
+                m_off += M_cur;
+                M_cur = M_def;
+            }
+
+            shift(-m_off, -m_off);
+            length(m_u, m_v);
+        };
 
 #if TBLIS_ENABLE_TBB
 
-#if TBLIS_SIMPLE_TBB
-        tbb::task_group tg;
-#endif
-
-        for (int tid = 0;tid < distribute;tid++)
+        if (distribute > 1)
         {
+            tbb::task_group tg;
 
-#if TBLIS_SIMPLE_TBB
-        tg.run([&,tid,this]
+            for (int tid = 0;tid < distribute;tid++)
+            {
+                tg.run([&,tid]
+                {
+                    auto child = this->child;
+                    auto A_ = A;
+                    auto B_ = B;
+                    auto C_ = C;
+                    body(tid, child, A_, B_, C_);
+                });
+            }
+
+            tg.wait();
+        }
+        else
         {
+            body(0, child, A, B, C);
+        }
 
-        auto child = this->child;
-        auto A = A_;
-        auto B = B_;
-        auto C = C_;
-        auto beta = beta_;
-#endif
-
-#else
+#else //TBLIS_ENABLE_TBB
 
         if (!ganged)
         {
@@ -69,67 +122,9 @@ struct partition
             ganged = true;
         }
 
-        int tid = subcomm.gang_num();
+        body(subcomm.gang_num(), child, A, B, C);
 
-        auto& A = A_;
-        auto& B = B_;
-        auto& C = C_;
-        auto& beta = beta_;
-
-#endif
-
-        len_type m_first, m_last;
-        std::tie(m_first, m_last, std::ignore) =
-            communicator::distribute(distribute, tid, m, M_iota);
-
-        auto length = [&](len_type m_u, len_type m_v)
-        {
-            (Dim == DIM_M ? A.length(0, m_u) : Dim == DIM_N ? B.length(1, m_u) : A.length(1, m_u));
-            (Dim == DIM_M ? C.length(0, m_v) : Dim == DIM_N ? C.length(1, m_v) : B.length(0, m_v));
-        };
-
-        auto shift = [&](len_type m_u, len_type m_v)
-        {
-            (Dim == DIM_M ? A.shift(0, m_u) : Dim == DIM_N ? B.shift(1, m_u) : A.shift(1, m_u));
-            (Dim == DIM_M ? C.shift(0, m_v) : Dim == DIM_N ? C.shift(1, m_v) : B.shift(0, m_v));
-        };
-
-        len_type m_off = m_first;
-        len_type m_len = m_last-m_first;
-
-        shift(m_off, m_off);
-
-        len_type M_cur = (m_len%M_def <= M_over ? M_max : M_def);
-
-        while (m_off < m_last)
-        {
-            len_type m_loc = std::min(m_last-m_off, M_cur);
-            length(m_loc, m_loc);
-
-            child(subcomm, cfg, alpha, A, B, beta, C);
-            if (Dim == DIM_K) beta = 1.0;
-
-            shift(M_cur, M_cur);
-            m_off += M_cur;
-            M_cur = M_def;
-        }
-
-        shift(-m_off, -m_off);
-        length(m_u, m_v);
-
-#if TBLIS_ENABLE_TBB
-
-#if TBLIS_SIMPLE_TBB
-        });
-#endif
-
-        }
-
-#if TBLIS_SIMPLE_TBB
-        tg.wait();
-#endif
-
-#endif
+#endif //TBLIS_ENABLE_TBB
     }
 };
 

93f293f6d982f3e6e3add6c2903971d24a871f9f
Devin Matthews <damatthews@smu.edu>
2020-04-01 12:39:51 -0500

    Update test_install.cxx.

--- test/test_install.cxx
+++ test/test_install.cxx
@@ -4,9 +4,9 @@ using namespace tblis;
 
 int main()
 {
-    tensor<double> A({10, 4, 3});
-    tensor<double> B({3, 4, 6, 7});
-    tensor<double> C({7, 10, 6});
+    varray<double> A({10, 4, 3});
+    varray<double> B({3, 4, 6, 7});
+    varray<double> C({7, 10, 6});
 
-    mult<double>(1.0, A, "abc", B, "cbde", 0.0, C, "ead");
+    mult(1.0, A, "abc", B, "cbde", 0.0, C, "ead");
 }

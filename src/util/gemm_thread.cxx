50a350cd03a8a34c37834f6bd4b44d4326dc5d5a
Devin Matthews <damatthews@smu.edu>
2020-04-02 15:25:53 -0500

    Cache env. vars. in gemm_thread.

--- src/util/gemm_thread.cxx
+++ src/util/gemm_thread.cxx
@@ -31,10 +31,15 @@ gemm_thread_config make_gemm_thread_config(type_t type, const config& cfg,
         }
     }
 
-    jc_nt = envtol("BLIS_JC_NT", jc_nt);
-    ic_nt = envtol("BLIS_IC_NT", ic_nt);
-    jr_nt = envtol("BLIS_JR_NT", jr_nt);
-    ir_nt = envtol("BLIS_IR_NT", ir_nt);
+    static auto jc_nt_env = envtol("BLIS_JC_NT", -1);
+    static auto ic_nt_env = envtol("BLIS_IC_NT", -1);
+    static auto jr_nt_env = envtol("BLIS_JR_NT", -1);
+    static auto ir_nt_env = envtol("BLIS_IR_NT", -1);
+
+    jc_nt = jc_nt_env != -1 ? jc_nt_env : jc_nt;
+    ic_nt = ic_nt_env != -1 ? ic_nt_env : ic_nt;
+    jr_nt = jr_nt_env != -1 ? jr_nt_env : jr_nt;
+    ir_nt = ir_nt_env != -1 ? ir_nt_env : ir_nt;
 
     TBLIS_ASSERT(ir_nt*jr_nt*ic_nt*jc_nt == nthread);
 

8c9349ee345b721a3dbfef04665e44961da2e955
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2021-04-17 01:18:49 +0900

    Fix Typo: NC/KC Block

--- src/configs/thunderx2/config.hpp
+++ src/configs/thunderx2/config.hpp
@@ -17,13 +17,13 @@ TBLIS_BEGIN_CONFIG(thunderx2)
     TBLIS_CONFIG_GEMM_NR(  12,    8, _, _)
     TBLIS_CONFIG_GEMM_KR(   4,    4, _, _)
     TBLIS_CONFIG_GEMM_MC( 120,  120, _, _)
-    TBLIS_CONFIG_GEMM_NC( 640,  240, _, _)
-    TBLIS_CONFIG_GEMM_KC(3072, 3072, _, _)
+    TBLIS_CONFIG_GEMM_NC(3072, 3072, _, _)
+    TBLIS_CONFIG_GEMM_KC( 640,  240, _, _)
                         
-    // TBLIS_CONFIG_M_THREAD_RATIO(_,3,_,_)
-    // TBLIS_CONFIG_N_THREAD_RATIO(_,2,_,_)
-    // TBLIS_CONFIG_MR_MAX_THREAD(_,1,_,_)
-    // TBLIS_CONFIG_NR_MAX_THREAD(_,4,_,_)
+    TBLIS_CONFIG_M_THREAD_RATIO(_,8,_,_)
+    TBLIS_CONFIG_N_THREAD_RATIO(_,2,_,_)
+    TBLIS_CONFIG_MR_MAX_THREAD(_,1,_,_)
+    TBLIS_CONFIG_NR_MAX_THREAD(_,4,_,_)
 
     TBLIS_CONFIG_GEMM_WRAP_UKR(bli_sgemm_armv8a_asm_8x12,
                                bli_dgemm_armv8a_asm_6x8,
